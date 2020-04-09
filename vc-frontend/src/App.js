ddf7b16e1fe2133ef4fb734809e838d7949e6d27
the-greenwell <ant.greenwell2@gmail.com>
2020-04-09 12:54:19 -0700

    routing implemented

--- vc-frontend/src/App.js
+++ vc-frontend/src/App.js
@@ -2,14 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <ul>
+            <li><Link to='/home'>Home</Link></li>
+            <li><Link to='/closet'>Closet</Link></li>
+          </ul>
+        </header>
+        <Switch>
+          <Route path='/home' exact component={Home} />
+          <Route path='/closet' component={Closet} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+const Home = () => (
+  <>
+    <h1>Home</h1>
+  </>
+)
+
+const Closet = () => (
+  <>
+    <h1>Closet</h1>
+  </>
+)
 
 export default App;
